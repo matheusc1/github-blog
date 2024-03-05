dea704bd4728c8c1817a071b8721efca9c77d8ad
matheusc1 <cardoso.matheusbs@gmail.com>
2024-03-04 22:49:00 -0300

    style: add color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: '#3294F8',
+        title: '#E7EDF4',
+        subtitle: '#C4D4E3',
+        text: '#AFC2D4',
+        span: '#7B96B2',
+        label: '#3A536B',
+        border: '#1C2F41',
+        post: '#112131',
+        profile: '#0B1B2B',
+        background: '#071422',
+        input: '#040F1A'
+      }
+    },
   },
   plugins: [],
 }
