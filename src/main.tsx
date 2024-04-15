eebccb3c9febeec26d6a74515af9e654f45311d0
matheusc1 <cardoso.matheusbs@gmail.com>
2024-04-15 15:18:43 -0300

    feat: configure application navigation routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app.tsx'
+import { Post } from './pages/post.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/post",
+    element: <Post />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
