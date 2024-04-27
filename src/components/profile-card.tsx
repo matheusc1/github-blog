f82602de0e71ad68b1eb77caf2a4bd6fac23cac8
matheusc1 <cardoso.matheusbs@gmail.com>
2024-04-27 13:09:41 -0300

    feat: add profile card information via github api

--- src/components/profile-card.tsx
+++ src/components/profile-card.tsx
@@ -1,40 +1,86 @@
-import { LucideArrowUpRightFromSquare, LucideGithub, LucideBuilding, LucideUsersRound } from "lucide-react";
+import axios from "axios"
+import { 
+  LucideArrowUpRightFromSquare,
+  LucideGithub,
+  LucideBuilding,
+  LucideUsersRound
+} from "lucide-react"
+import { useEffect, useState } from "react"
+
+interface User {
+  avatarUrl: string
+  name: string
+  bio: string
+  username: string
+  company?: string
+  followers: number
+  url: string
+}
 
 export function ProfileCard() {
+  const [userInfo, setUserInfo] = useState<User>()
+
+  useEffect(() => {
+    async function getUserInfo() {
+      try {
+        const { data } = await axios.get('https://api.github.com/users/matheusc1')
+
+        const userInfoData = {
+          avatarUrl: data.avatar_url,
+          name: data.name,
+          bio: data.bio,
+          username: data.login,
+          followers: data.followers,
+          company: data?.company,
+          url: data.html_url
+        }
+        setUserInfo(userInfoData)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getUserInfo()
+  }, [])
+
+  console.log(userInfo)
+
   return (
     <div className="flex justify-start bg-profile rounded-[10px] p-8 w-full max-w-864 -mt-24 gap-8 shadow-md">
-        <img src="https://github.com/matheusc1.png" className='size-36 rounded-lg' alt="" />
+        <img src={userInfo?.avatarUrl} className='size-36 rounded-lg' alt="" />
 
         <div className="flex flex-col justify-between w-full">
 
           <div className="flex justify-between items-center">
-            <p className="font-bold text-2xl text-title">Matheus Cardoso</p>
+            <p className="font-bold text-2xl text-title">{userInfo?.name}</p>
 
             <div className="flex gap-2 items-center cursor-pointer  border-b border-transparent hover:border-blue hover:border-b">
-              <span className="uppercase text-blue font-bold text-xs">Github</span>
+              <a href={userInfo?.url} className="uppercase text-blue font-bold text-xs">Github</a>
               <LucideArrowUpRightFromSquare strokeWidth={3} className="size-3 text-blue" />
             </div>
           </div>
 
           <p className="text-base text-text">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum voluptas molestias culpa vero
-            totam facere qui tempore provident!
+            {userInfo?.bio}
           </p>
 
           <div className="flex space-x-8">
             <div className="flex items-center gap-1">
               <LucideGithub className="size-4 text-label" />
-              <span className="text-subtitle">matheusc1</span>
+              <span className="text-subtitle">{userInfo?.username}</span>
             </div>
 
-            <div className="flex items-center gap-1">
-              <LucideBuilding className="size-4 text-label" />
-              <span className="text-subtitle">Rocketseat</span>
-            </div>
+            {
+              userInfo?.company ?
+              <div className="flex items-center gap-1">
+                <LucideBuilding className="size-4 text-label" />
+                <span className="text-subtitle">Rocketseat</span>
+              </div> : ""
+            }
 
             <div className="flex items-center gap-1">
               <LucideUsersRound className="size-4 text-label" />
-              <span className="text-subtitle">matheusc1</span>
+              <span className="text-subtitle">{userInfo?.followers} seguidores</span>
             </div>
           </div>
           
