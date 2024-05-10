b7934a20b4b96f04b0b57fab1d40c2da95a08ad6
matheusc1 <cardoso.matheusbs@gmail.com>
2024-05-09 21:16:05 -0300

    configure react markdown on the post page

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -7,6 +7,25 @@ import {
 } from 'lucide-react'
 import cover from '../assets/header-cover.png'
 import { Link } from 'react-router-dom'
+import Markdown from 'react-markdown'
+
+const markdown = `
+**Programming languages all have built-in data structures,
+but these often differ from one language to another.**
+This article attempts to list the built-in data structures available in JavaScript and what properties they have.
+These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.  
+&nbsp;  
+
+[Dynamic Typing](https://github.com/matheusc1)  
+JavaScript is a loosely typed and dynamic language. Variables in JavaScript are not directly 
+associated with any particular value type, and any variable can be assigned (and re-assigned) values of all types:  
+&nbsp;  
+~~~js
+let foo = 42;       //   foo is now a number  
+foo = 'bar';        //   foo is now a string  
+foo = true;         //   foo is now a boolean
+~~~
+`
 
 export function Post() {
   return (
@@ -56,21 +75,19 @@ export function Post() {
       </div>
 
       <div className="w-full max-w-864 p-8 flex flex-col">
-        <p className="mb-6 text-text">
-          <strong>Programming languages all have built-in data structures, but these often differ from one language to another. </strong> 
-          This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.
-        </p>
-
-        <span className="text-blue underline">Dynamic typing</span>
-        <p>
-          JavaScript is a loosely typed and dynamic language. Variables in JavaScript are not directly associated with any particular value type, and any variable can be assigned (and re-assigned) values of all types:
-        </p>
-
-        <div className="bg-post p-4 flex flex-col items-start justify-center rounded mt-6 mb-20">
-          <p>let foo = 42; <span className="ml-6 text-[#4F6173]">// foo is now a number</span></p>
-          <p>foo = bar; <span className="ml-10 text-[#4F6173]">// foo is now a string</span></p>
-          <p>foo = true; <span className="ml-8 text-[#4F6173]">// foo is now a boolean</span></p>
-        </div>
+        <Markdown  components={{
+          a: ({...props}) => <a className="text-blue underline decoration-blue" {...props} />,
+          code: ({ inline, className, children, ...props }) => {
+          const match = /language-(\w+)/.exec(className || '');
+          return !inline && match ? (
+            <pre className={`p-4 bg-post rounded ${className}`} {...props}>
+              <code className={`${match[1]}`}>{children}</code>
+            </pre>
+          ) : (
+            <code>{children}</code>
+          );
+        }
+        }}>{markdown}</Markdown>
       </div>
     </div>
   )
