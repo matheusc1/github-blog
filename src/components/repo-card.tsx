f43cdb5d4387781307783df860e1539ce5c85355
matheusc1 <cardoso.matheusbs@gmail.com>
2024-05-19 16:46:43 -0300

    feat: configure dayjs and add properties to the repo-card component

--- src/components/repo-card.tsx
+++ src/components/repo-card.tsx
@@ -1,20 +1,31 @@
 import { Link } from "react-router-dom"
+import dayjs from "dayjs"
+import "dayjs/locale/pt-br"
+import relativeTime from "dayjs/plugin/relativeTime"
 
-export function RepoCard() {
+dayjs.extend(relativeTime)
+dayjs.locale('pt-br')
+
+interface RepoCardProps {
+  title: string
+  date: string
+  text: string
+}
+
+export function RepoCard(children: RepoCardProps) {
   return (
     <Link to="/post">
       <button className="flex flex-col text-left w-card bg-post rounded-lg h-64 p-8 overflow-hidden
       relative outline-none hover:ring-2 hover:ring-label focus-visible:ring-2 focus-visible:ring-blue">
         <div className="w-full flex justify-between">
           <p className="text-xl text-title font-bold w-60 -mt-1">
-            JavaScript data types and data structures
+            {children.title}
           </p>
-          <span className="text-sm text-span">Há 1 dia</span>
+          <span className="text-sm text-span">{dayjs().to(children.date)}</span>
         </div>
 
         <p className="text-span mt-5 text-ellipsis max-w-[352px] line-clamp-4">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus laborum minima iste quaerat minus numquam veniam quasi aut dolore. Exercitationem autem amet commodi.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et sed, sint ipsam cupiditate praesentium tempora libero quisquam enim dolorem dolore voluptates quasi ut mollitia recusandae sapiente officia illum, dolores commodi.
+          {children.text}
         </p>
       </button>
     </Link>
