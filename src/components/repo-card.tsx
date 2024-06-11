bb369ef37a3ca02aa93de60d6a09c5c1f4443c5a
matheusc1 <cardoso.matheusbs@gmail.com>
2024-06-11 18:39:50 -0300

    chore: mall adjustments to component to align with recent implementations

--- src/components/repo-card.tsx
+++ src/components/repo-card.tsx
@@ -2,31 +2,31 @@ import { Link } from "react-router-dom"
 import dayjs from "dayjs"
 import "dayjs/locale/pt-br"
 import relativeTime from "dayjs/plugin/relativeTime"
+import Markdown from "react-markdown"
+import { Issue } from "../app"
 
 dayjs.extend(relativeTime)
 dayjs.locale('pt-br')
 
 interface RepoCardProps {
-  title: string
-  date: string
-  text: string
+  issue: Issue
 }
 
-export function RepoCard(children: RepoCardProps) {
+export function RepoCard({ issue }: RepoCardProps) {
   return (
-    <Link to="/post">
+    <Link to={`/post/${issue.number}`}>
       <button className="flex flex-col text-left w-card bg-post rounded-lg h-64 p-8 overflow-hidden
       relative outline-none hover:ring-2 hover:ring-label focus-visible:ring-2 focus-visible:ring-blue">
         <div className="w-full flex justify-between">
           <p className="text-xl text-title font-bold w-60 -mt-1">
-            {children.title}
+            {issue.title}
           </p>
-          <span className="text-sm text-span">{dayjs().to(children.date)}</span>
+          <span className="text-sm text-span">{dayjs().to(issue.created_at)}</span>
         </div>
 
-        <p className="text-span mt-5 text-ellipsis max-w-[352px] line-clamp-4">
-          {children.text}
-        </p>
+        <Markdown className="text-span mt-5 text-ellipsis max-w-[352px] line-clamp-4">
+          {issue.body}
+        </Markdown>
       </button>
     </Link>
   )
