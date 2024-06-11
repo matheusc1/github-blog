a02a798694e8728217c17d8175ce71db10883937
matheusc1 <cardoso.matheusbs@gmail.com>
2024-06-11 18:35:20 -0300

    feat: configure the issue request for the github API

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,48 @@
+import axios from 'axios'
 import cover from './assets/header-cover.png'
 import { ProfileCard } from './components/profile-card'
 import { RepoCard } from './components/repo-card'
-import { useForm, SubmitHandler } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
+import { useState } from 'react'
+
+export interface Issue {
+  number: number
+  title: string
+  created_at: string
+  body: string
+}
+
+interface SearchIssuesResponse {
+  total_count: number
+  incomplete_results: boolean
+  items: Issue[]
+}
 
 type Input = {
   searchText: string
 }
 
 export function App() {
-  const { 
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<Input>()
+  const [issues, setIssues] = useState<SearchIssuesResponse | null>(null)
 
-  const onSubmit: SubmitHandler<Input> = (data) => console.log(data)
+  const {register, handleSubmit, formState: { errors }} = useForm<Input>({
+    mode: "onBlur"
+  })
 
-  console.log(watch("searchText"))
+  async function onSubmit(data: Input) {
+    try {
+      const response = await axios.get('https://api.github.com/search/issues',{
+        params: {
+          q: `repo:${data.searchText}`,
+        }
+      })
+
+      setIssues(response.data)
+    } catch (error) {
+      console.error('Erro na requisição:', error)
+      setIssues(null)
+    }
+  }
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-start">
@@ -29,27 +54,38 @@ export function App() {
 
       <div className="w-full max-w-864 flex justify-between items-center mt-19">
         <p className="text-subtitle text-lg font-bold">Publicações</p>
-        <span className="text-span text-sm">6 publicações</span>
+
+        {issues && issues?.total_count > 0 && (
+          <span className="text-span text-sm">
+          {issues?.total_count}
+          {issues?.total_count === 1 ? ' Publicação' : ' Publicações'}
+        </span>
+        )}
+        
       </div>
 
       <form className='w-full max-w-864' onSubmit={handleSubmit(onSubmit)}>
         <input
           type="text"
-          placeholder="Buscar conteúdo"
+          placeholder="Buscar conteúdo: username/repository"
           className="mt-3 w-full border border-border rounded-md py-3 px-4 bg-input
           text-text placeholder:text-label outline-none focus-visible:ring-1 focus-visible:ring-blue"
-          {...register("searchText")}
+          {...register("searchText", { required: "Este campo é obrigatório" })}
         />
-        {errors.searchText && <span className='text-red-500 text-xs'>Este campo é obrigatório</span>}
+
+        <p className={`mt-2 text-red-500 text-xs ${errors.searchText ? '' : 'invisible'}`}>
+          Este campo é obrigatório
+        </p>
       </form>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-12 mb-20">
-        <RepoCard />
-        <RepoCard />
-        <RepoCard />
-        <RepoCard />
-        <RepoCard />
-        <RepoCard />
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-10 mb-20">
+        {issues && issues.items.length > 0 ? (
+          issues.items.map(item => (
+            <RepoCard key={item.number} issue={item} />
+          ))
+        ) : (
+          <div className='col-span-full' />
+        )}
       </div>
 
     </div>
